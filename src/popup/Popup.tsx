2f86364fc950324df10b148ca7ab78090216bb24
Berker Gungor <yasarberkergungor@gmail.com>
2023-04-03 08:17:20 +0300

    feat: implement shortcut recorder input
    - https://github.com/Lambdaphile/shorty/issues/4

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
+import ShortcutRecorder from './components/ShortcutRecorder'
 import './Popup.css'
 
 function App() {
-  const [crx, setCrx] = useState('create-chrome-ext')
-
   return (
     <main>
-      <h3>Popup Page!</h3>
-
-      <h6>v 0.0.0</h6>
-
-      <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">
-        Power by {crx}
-      </a>
+      <ShortcutRecorder />
     </main>
   )
 }
